669052bad0fa22cc59582abb5c8a24c442dedd5e
denist2322 <parksomang125@gmail.com>
2022-08-31 09:31:10 +0900

    5강 이벤트

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 
 function Header(props){
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 
 function Nav(props){
 
   const tmp = props.topics;
-  const lis = tmp.map((tmp) => <li key={tmp.id}>{tmp.title}</li>);
-  
-  // for(let i=0; i< props.topics.length; i++){
-  //     let t = props.topics[i];
-  //     lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
-  //   }
-  
+  const lis = tmp.map((tmp) => <li key={tmp.id}> <a id={tmp.id} href={'/read/' + tmp.id} onClick={event=>{
+    event.preventDefault();
+    props.onChangeMode(event.target.id);
+  }}>{tmp.title}</a></li>);
 
   return <nav>
   <ol>
@@ -39,8 +39,12 @@ function App() {
   ]
   return (
     <div className="App">
-      <Header title = "WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title = "WEB" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="welcome" body="Hello"></Article>
     </div>
   );
