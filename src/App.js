8fc814a132227399c19ac050fdb00fa5f06ec569
denist2322 <parksomang125@gmail.com>
2022-08-31 11:47:01 +0900

    6강 state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import './App.css';
 
 function Header(props){
@@ -14,7 +15,7 @@ function Nav(props){
   const tmp = props.topics;
   const lis = tmp.map((tmp) => <li key={tmp.id}> <a id={tmp.id} href={'/read/' + tmp.id} onClick={event=>{
     event.preventDefault();
-    props.onChangeMode(event.target.id);
+    props.onChangeMode(Number(event.target.id));
   }}>{tmp.title}</a></li>);
 
   return <nav>
@@ -32,20 +33,43 @@ function Article(props){
 }
 
 function App() {
+  // const _mode = useState('WELCOME');
+  // console.log("mode: ",mode);
+  // console.log("setMode: ",setMode);
+
+  const [mode,setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
   const topics =  [
     {id:1 , title:'html', body:'html is ...'},
     {id:2 , title:'css', body:'css is ...'},
     {id:3 , title:'javascript', body:'javascript is ...'}
   ]
+  let content = null;
+
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }else if (mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
+
   return (
     <div className="App">
       <Header title = "WEB" onChangeMode={()=>{
-        alert('Header');
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title="welcome" body="Hello"></Article>
+      {content}
     </div>
   );
 }
