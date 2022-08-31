1c8c5d1690d7896693784a16d94376b0e3473c7d
denist2322 <parksomang125@gmail.com>
2022-08-31 22:18:21 +0900

    9강 DELETE

--- src/App.js
+++ src/App.js
@@ -100,10 +100,22 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
-    contextControll = <li><a href={"/update/" + id} onClick={event=>{
+    contextControll = <>
+    <li><a href={"/update/" + id} onClick={event=>{
       event.preventDefault();
       setMode("UPDATE");
-    }}>Update</a></li>;
+    }}>Update</a></li>
+    <li><input type="button" value="Delete" onClick={()=>{
+      const newTopics = [];
+      for(let i =0; i<topics.length; i++){
+        if(topics[i].id !== id){
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode('WELCOME');
+    }}/></li>
+    </>;
 
   }
   else if (mode === 'CREATE'){
@@ -162,6 +174,7 @@ function App() {
         }}>Create</a></li>
 
         {contextControll}
+
       </ul>
     </div>
   );
